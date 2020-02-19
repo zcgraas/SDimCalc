dbfa7751e24931457c670a74de4520627a38ae9a
zcgraas <61208293+zcgraas@users.noreply.github.com>
2020-02-18 19:59:53 -0500

    Add files via upload

---
+++ Lab3/Lab3/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace Lab3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblRadius = new System.Windows.Forms.Label();
+            this.txtRadius = new System.Windows.Forms.TextBox();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.lblDiameter = new System.Windows.Forms.Label();
+            this.lblSurfaceArea = new System.Windows.Forms.Label();
+            this.lblVolume = new System.Windows.Forms.Label();
+            this.txtDiameter = new System.Windows.Forms.TextBox();
+            this.txtSurfaceArea = new System.Windows.Forms.TextBox();
+            this.txtVolume = new System.Windows.Forms.TextBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Lab3.Properties.Resources.Sphere_and_Ball;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.MaximumSize = new System.Drawing.Size(150, 150);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(150, 150);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lblRadius
+            // 
+            this.lblRadius.AutoSize = true;
+            this.lblRadius.Location = new System.Drawing.Point(168, 74);
+            this.lblRadius.Name = "lblRadius";
+            this.lblRadius.Size = new System.Drawing.Size(90, 13);
+            this.lblRadius.TabIndex = 1;
+            this.lblRadius.Text = "Radius of sphere:";
+            // 
+            // txtRadius
+            // 
+            this.txtRadius.Location = new System.Drawing.Point(264, 74);
+            this.txtRadius.Name = "txtRadius";
+            this.txtRadius.Size = new System.Drawing.Size(100, 20);
+            this.txtRadius.TabIndex = 2;
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Location = new System.Drawing.Point(275, 100);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(75, 23);
+            this.btnCalc.TabIndex = 3;
+            this.btnCalc.Text = "Calculate";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // lblDiameter
+            // 
+            this.lblDiameter.AutoSize = true;
+            this.lblDiameter.Location = new System.Drawing.Point(32, 221);
+            this.lblDiameter.Name = "lblDiameter";
+            this.lblDiameter.Size = new System.Drawing.Size(52, 13);
+            this.lblDiameter.TabIndex = 4;
+            this.lblDiameter.Text = "Diameter:";
+            // 
+            // lblSurfaceArea
+            // 
+            this.lblSurfaceArea.AutoSize = true;
+            this.lblSurfaceArea.Location = new System.Drawing.Point(12, 277);
+            this.lblSurfaceArea.Name = "lblSurfaceArea";
+            this.lblSurfaceArea.Size = new System.Drawing.Size(72, 13);
+            this.lblSurfaceArea.TabIndex = 5;
+            this.lblSurfaceArea.Text = "Surface Area:";
+            // 
+            // lblVolume
+            // 
+            this.lblVolume.AutoSize = true;
+            this.lblVolume.Location = new System.Drawing.Point(39, 330);
+            this.lblVolume.Name = "lblVolume";
+            this.lblVolume.Size = new System.Drawing.Size(45, 13);
+            this.lblVolume.TabIndex = 6;
+            this.lblVolume.Text = "Volume:";
+            // 
+            // txtDiameter
+            // 
+            this.txtDiameter.Location = new System.Drawing.Point(91, 221);
+            this.txtDiameter.Name = "txtDiameter";
+            this.txtDiameter.Size = new System.Drawing.Size(100, 20);
+            this.txtDiameter.TabIndex = 7;
+            // 
+            // txtSurfaceArea
+            // 
+            this.txtSurfaceArea.Location = new System.Drawing.Point(91, 277);
+            this.txtSurfaceArea.Name = "txtSurfaceArea";
+            this.txtSurfaceArea.Size = new System.Drawing.Size(100, 20);
+            this.txtSurfaceArea.TabIndex = 8;
+            // 
+            // txtVolume
+            // 
+            this.txtVolume.Location = new System.Drawing.Point(91, 330);
+            this.txtVolume.Name = "txtVolume";
+            this.txtVolume.Size = new System.Drawing.Size(100, 20);
+            this.txtVolume.TabIndex = 9;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Lab3.Properties.Resources.Sphere_and_Ball;
+            this.pictureBox2.Location = new System.Drawing.Point(222, 200);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(150, 150);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 10;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 361);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.txtVolume);
+            this.Controls.Add(this.txtSurfaceArea);
+            this.Controls.Add(this.txtDiameter);
+            this.Controls.Add(this.lblVolume);
+            this.Controls.Add(this.lblSurfaceArea);
+            this.Controls.Add(this.lblDiameter);
+            this.Controls.Add(this.btnCalc);
+            this.Controls.Add(this.txtRadius);
+            this.Controls.Add(this.lblRadius);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Lab 3";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblRadius;
+        private System.Windows.Forms.TextBox txtRadius;
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.Label lblDiameter;
+        private System.Windows.Forms.Label lblSurfaceArea;
+        private System.Windows.Forms.Label lblVolume;
+        private System.Windows.Forms.TextBox txtDiameter;
+        private System.Windows.Forms.TextBox txtSurfaceArea;
+        private System.Windows.Forms.TextBox txtVolume;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ Lab3/Lab3/Form1.cs
@@ -0,0 +1,60 @@
+﻿//Grading ID: R1161
+//Lab 3
+//2/9/20
+//199-01
+//This program is designed to calculate the radius, diameter, and volume of a sphere when given the radius.
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            double radius;//variable for storing input radius
+            double areap;//power that area calc needs to be raised to
+            double volumep;//power that volume calc needs to be raised to
+            double diameter;//store diameter calc
+            double area;//store area calc
+            double volume;//store volume calc
+          
+
+            areap = 2;//setting areap = 2
+            volumep = 3;//setting volumep = 3
+            radius = Convert.ToDouble(txtRadius.Text);//setting radius to user input
+
+            diameter = radius * 2;//calculation for diameter
+            area = 4 * Math.PI * (Math.Pow(radius, areap));//calculation for area
+            volume = (4 * Math.PI * (Math.Pow(radius, volumep))) / 3;//calculation for volume;
+
+           
+
+            txtDiameter.Text = diameter.ToString("0.00");//converts diameter to string and displays in textbox
+            txtSurfaceArea.Text = area.ToString("0.00");//converts surface area to a string and displays in textbox
+            txtVolume.Text = volume.ToString("0.00");//converts volume to a string and displays in textbox
+
+
+
+
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
